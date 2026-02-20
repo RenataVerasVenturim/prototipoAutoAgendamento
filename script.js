5ae387eac2c57c72a3df4c356d749295f8ab05bf
Renata Veras Venturim <129551549+RenataVerasVenturim@users.noreply.github.com>
2026-02-20 15:03:54 -0300

    Atualização do protótipo

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-<!--
-Direitos Autorais (c) 2025 Renata Veras Venturim
-Licença MIT
 
-A permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia deste software
-e dos arquivos de documentação associados (o "Software"), para negociar no Software sem restrições,
-incluindo, sem limitação, os direitos de uso, cópia, modificação, fusão, publicação, distribuição,
-sublicenciamento e/ou venda de cópias do Software, e permitir que outros o façam, sujeitas às seguintes condições:
-
-O aviso de direitos autorais acima e este aviso de permissão devem ser incluídos em todas as cópias ou partes substanciais do Software.
-
-**Atribution Clause:** Os usuários deste software devem manter os créditos originais e as atribuições do projeto em quaisquer cópias ou derivações do Software.
-
-O SOFTWARE É FORNECIDO "COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU IMPLÍCITA,
-INCLUINDO, MAS NÃO SE LIMITANDO ÀS GARANTIAS DE COMERCIALIZAÇÃO, ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO E NÃO VIOLAÇÃO.
-EM NENHUM CASO OS AUTORES OU DETENTORES DOS DIREITOS AUTORAIS SERÃO RESPONSÁVEIS POR QUALQUER REIVINDICAÇÃO,
-DANOS OU OUTRAS RESPONSABILIDADES, SEJA EM AÇÃO DE CONTRATO, DELITO OU DE OUTRA FORMA, DECORRENTES DE,
-OU EM CONEXÃO COM O SOFTWARE OU O USO OU OUTRAS NEGOCIAÇÕES NO PROGRAMA.
--->
-
-<script>
 /* ===========================
    Variáveis globais
 =========================== */
@@ -272,4 +252,4 @@ google.script.run.withSuccessHandler(function(calendarId) {
   window.CALENDAR_ID = calendarId; // salvar para usar em outros scripts
 }).getCalendarId();
 
-</script>
+
